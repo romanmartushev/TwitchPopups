a2867e2d7f2c0da68e81c3368a4783a6422f0cb1
Roman <romanm@paulbunyan.net>
2022-05-06 10:34:56 -0500

    move to local scripts
    - added font
    - text to speech now can be captured from the browser

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ export default new Vue({
     alertSoundFile: 'assets/sounds/alert.mp3',
 	},
 	mounted() {
-
     this.activeCommands = {
       '!alert': this.alertCommand,
       '!spotlight': this.spotlightCommand,
@@ -37,28 +36,19 @@ export default new Vue({
       const command =  rawText.indexOf(" ") > -1 ? rawText.substring(0, rawText.indexOf(" ")) : rawText;
 
       if (command in this.activeCommands) {
-        this.activeCommands[command](context, rawText);
+        this.activeCommands[command](context, rawText.substring(command.length));
       } else {
         this.onOtherMessages(context, rawText);
       }
     },
     onCheerHandler(channel, userstate, message) {
-      const speech = new SpeechSynthesisUtterance(message);
-      speech.addEventListener('end', function (event) {
-        setTimeout(function() {
-          $("#tts").css('display', 'none');
-        }, 250);
-      });
-      $("#tts").css('display', 'block');
-      setTimeout(function() {
-        window.speechSynthesis.speak(speech);
-      }, 250);
+      this.textToSpeech(message);
     },
     alertCommand(context, textContent) {
       if (context.mod || context.subscriber) {
         this.spotlightUser = "";
-        const formattedText = popup.formatEmotes(textContent, context.emotes, true).substr(7);
-        popup.showText(formattedText);
+        const formattedText = this.formatEmotes(textContent, context.emotes);
+        this.showText(formattedText);
         if (this.playAlertSound) {
           new Audio(this.alertSoundFile).play();
         }
@@ -66,34 +56,76 @@ export default new Vue({
     },
     spotlightCommand(context, textContent) {
       if (context.mod || context.subscriber) {
-        this.spotlightUser = textContent.substr(12).toLowerCase();
+        console.log(textContent);
+        this.spotlightUser = textContent.substring(2).toLowerCase();
         if (this.spotlightUser.length === 0) {
-          popup.showText('');
+          this.showText('');
         } else {
-          popup.showText(`${this.spotlightEmoji} Welcome ${this.spotlightUser} to the party crew!`);
+          this.showText(`${this.spotlightEmoji} Welcome ${this.spotlightUser} to the party crew!`);
         }
       }
     },
     finCommand(context, textContent) {
       if (context.mod || context.subscriber) {
-        const text = textContent.substr(5);
-        const speech = new SpeechSynthesisUtterance(text);
-        speech.addEventListener('end', function (event) {
-          setTimeout(function() {
-            $("#tts").css('display', 'none');
-          }, 250);
-        });
-        $("#tts").css('display', 'block');
-        setTimeout(function() {
-          window.speechSynthesis.speak(speech);
-        }, 250);
+        this.textToSpeech(textContent);
       }
     },
     onOtherMessages(context, textContent) {
       if (context.username === this.spotlightUser) {
-        const formattedText = popup.formatEmotes(textContent, context.emotes, false);
-        popup.showText(`${this.spotlightEmoji} ${context['display-name']}: ${formattedText}`);
+        const formattedText = this.formatEmotes(textContent, context.emotes);
+        this.showText(`${this.spotlightEmoji} ${context['display-name']}: ${formattedText}`);
       }
+    },
+    formatEmotes(message, emotes) {
+      let newMessage = $($.parseHTML(message)).text().split("");
+
+      //replace any twitch emotes in the message with img tags for those emotes
+      for (let emoteIndex in emotes) {
+        const emote = emotes[emoteIndex];
+        for (let charIndexes in emote) {
+          let emoteIndexes = emote[charIndexes];
+          if (typeof emoteIndexes == "string") {
+            emoteIndexes = emoteIndexes.split("-");
+            emoteIndexes = [parseInt(emoteIndexes[0]), parseInt(emoteIndexes[1])];
+            for (let i = emoteIndexes[0]; i <= emoteIndexes[1]; ++i) {
+              newMessage[i] = "";
+            }
+            newMessage[emoteIndexes[0]] = `<img class="emoticon" src="https://static-cdn.jtvnw.net/emoticons/v2/${emoteIndex}/default/dark/3.0"/>`;
+          }
+        }
+      }
+      return newMessage.join("");
+    },
+    showText(text) {
+      if (text.length === 0) {
+        $("#popupbox").animate({ width: 0 }, 500);
+        $("#popuptext").animate({ "opacity": 0}, 700);
+        return;
+      }
+
+      $("#popuptext").html(text);
+
+      const textWidth = $("#popuptext").width();
+      $("#popupbox").animate({ width: textWidth + 30 }, 500);
+      $("#popuptext").animate({ "opacity": 1, "margin-left": "15px" }, 700);
+    },
+    textToSpeech(text) {
+      const src = "https://api.streamelements.com/kappa/v2/speech?voice=Justin&text=" + encodeURIComponent(text);
+
+      const audioTag = document.createElement("AUDIO");
+      audioTag.src = src;
+      document.body.appendChild(audioTag);
+      $("#tts").css('display', 'block');
+
+      setTimeout(function() {
+        audioTag.play();
+      }, 250);
+
+      audioTag.addEventListener("ended", () => {
+        setTimeout(function() {
+          $("#tts").css('display', 'none');
+        }, 250);
+      });
     }
   }
 });
