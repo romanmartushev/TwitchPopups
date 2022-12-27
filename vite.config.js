8b329a33cf2df18881737d1b51a1bd12c7691956
Roman <romanm@paulbunyan.net>
2022-12-27 11:56:41 -0600

    no need for alias

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import { fileURLToPath, URL } from "url";
 import tailwindcss from "tailwindcss";
 
 import { defineConfig } from "vite";
@@ -7,11 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
   server: {
     port: 3031,
   },
